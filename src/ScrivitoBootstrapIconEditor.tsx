80ce919398d1e7669e91de258e67bee4f752a5f4
David Császár <dcsaszar@users.noreply.github.com>
2025-12-02 16:18:55 +0100

    Disable editing in Scrivito changes view

--- src/ScrivitoBootstrapIconEditor.tsx
+++ src/ScrivitoBootstrapIconEditor.tsx
@@ -1,5 +1,5 @@
 import type { Widget, Obj } from 'scrivito'
-import { uiContext, canEdit, connect } from 'scrivito'
+import { uiContext, canEdit, connect, isInPlaceEditingActive } from 'scrivito'
 import { BootstrapIconPicker } from './BootstrapIconPicker.js'
 
 export const ScrivitoBootstrapIconEditor = connect(
@@ -27,13 +27,15 @@ export const ScrivitoBootstrapIconEditor = connect(
     const content = obj || page || widget
     const value = content.get(attributeName)
     const theme = uiContext()?.theme
+    const isEditable =
+      isInPlaceEditingActive() && canEdit(obj || page || widget.obj())
 
     return theme ? (
       <div className={`scrivito_${theme} scrivito-icon-editor`}>
         <div className="description">{description}</div>
         <div className="preview-title">{previewTitle ?? 'Preview'}</div>
         <BootstrapIconPicker
-          disabled={!canEdit(obj || page || widget.obj())}
+          disabled={!isEditable}
           onChange={(name) =>
             content.update({ [attributeName]: name ? `bi-${name}` : '' })
           }
